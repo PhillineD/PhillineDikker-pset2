36830658e0b32055625b6b4b6e0726faa0e2b0ae
Philline <fidik@live.nl>
2018-11-15 18:29:51 +0100

    Initial Project

---
+++ gradlew.bat
@@ -0,0 +1,45 @@
+<application>
+  <component name="ActionsCollector">
+    <counts>
+      <entry action="$Copy" count="4" />
+      <entry action="$Delete" count="10" />
+      <entry action="$Paste" count="21" />
+      <entry action="$SelectAll" count="1" />
+      <entry action="$Undo" count="387" />
+      <entry action="ActivateLogcatToolWindow" count="2" />
+      <entry action="ActivateProjectToolWindow" count="1" />
+      <entry action="ChangesView.AddUnversioned.From.Dialog" count="1" />
+      <entry action="ChangesView.Refresh" count="1" />
+      <entry action="ChangesView.Shelve" count="1" />
+      <entry action="CheckForUpdate" count="1" />
+      <entry action="CheckinFiles" count="8" />
+      <entry action="CheckinProject" count="31" />
+      <entry action="CloseProject" count="1" />
+      <entry action="CodeCompletion" count="2" />
+      <entry action="CollapseSelection" count="3" />
+      <entry action="CommentByLineComment" count="148" />
+      <entry action="CopyReference" count="2" />
+      <entry action="CreateRunConfiguration" count="1" />
+      <entry action="EditorBackSpace" count="4987" />
+      <entry action="EditorChooseLookupItem" count="22" />
+      <entry action="EditorChooseLookupItemReplace" count="482" />
+      <entry action="EditorCodeBlockEnd" count="1" />
+      <entry action="EditorCopy" count="233" />
+      <entry action="EditorCut" count="37" />
+      <entry action="EditorDelete" count="117" />
+      <entry action="EditorDeleteToWordEnd" count="7" />
+      <entry action="EditorDeleteToWordStart" count="2" />
+      <entry action="EditorDown" count="9528" />
+      <entry action="EditorEnter" count="985" />
+      <entry action="EditorEscape" count="1" />
+      <entry action="EditorIndentSelection" count="4" />
+      <entry action="EditorLeft" count="1513" />
+      <entry action="EditorLineEnd" count="1" />
+      <entry action="EditorLineStart" count="1" />
+      <entry action="EditorPaste" count="692" />
+      <entry action="EditorPasteSimple" count="1" />
+      <entry action="EditorRight" count="2393" />
+      <entry action="EditorSplitLine" count="7" />
+      <entry action="EditorTab" count="261" />
+      <entry action="EditorToggleColumnMode" count="3" />
+      <entry action="EditorToggleInsertState" count="2"
