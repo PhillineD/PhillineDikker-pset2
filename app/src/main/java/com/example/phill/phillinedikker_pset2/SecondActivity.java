2b40d7f8cff9faccfb9925edb0bc375b0f9859ff
Philline <fidik@live.nl>
2018-11-17 22:00:00 +0100

    Initial Project

--- app/src/main/java/com/example/phill/phillinedikker_pset2/SecondActivity.java
+++ app/src/main/java/com/example/phill/phillinedikker_pset2/SecondActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.view.LayoutInflater;
 
 
 import java.io.InputStream;
